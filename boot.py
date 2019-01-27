8709132fb42814641c24e026a2c3b161e35a4795
George Mamaladze <george@Georges-MacBook-Pro.local>
2019-01-27 21:57:44 +0100

    Added flashung light.

--- boot.py
+++ boot.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python
-
 import os
 import sys
 import signal
 import subprocess
 from time import sleep
-import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO
+import threading
 
 pro = None
 
@@ -16,21 +15,57 @@ def restart():
         os.killpg(os.getpgid(pro.pid), signal.SIGTERM)  # Send the signal to all the process groups
         sleep(1)
 
-    subprocess.call('if [ -d ./app ]; then rm -Rf ./mrbean-robot-master; fi', shell=True)
-    subprocess.call('wget https://github.com/gmamaladze/mrbean-robot/archive/master.zip', shell=True)
-    subprocess.call('unzip master.zip -d ./', shell=True)
-    subprocess.call('pip3 install -r ./mrbean-robot-master/requirements.txt', shell=True)
-    command='python3 ./mrbean-robot-master/main.py'
-    print('Starting new program.')
-    pro = subprocess.Popen(command, stdout=sys.stdout, stderr=subprocess.STDOUT, shell=True, preexec_fn=os.setsid)
+    e = flash()
+    try:
+        subprocess.call('if [ -d ./app ]; then rm -Rf ./mrbean-robot-master; fi', shell=True)
+        subprocess.call('wget https://github.com/gmamaladze/mrbean-robot/archive/master.zip', shell=True)
+        subprocess.call('unzip master.zip -d ./', shell=True)
+        subprocess.call('pip3 install -r ./mrbean-robot-master/requirements.txt', shell=True)
+        command='python3 ./mrbean-robot-master/main.py'
+        print('Starting new program.')
+        pro = subprocess.Popen(command, stdout=sys.stdout, stderr=subprocess.STDOUT, shell=True, preexec_fn=os.setsid)
+    finally:
+        e.set()
 
+pins={1:33,2:35,3:37,4:36}
+
+def leds_off():
+    for pin in pins:
+        GPIO.setup(pin,GPIO.OUT)
+        GPIO.output(pin, GPIO.LOW)
+
+def leds_red_on():
+    leds_off()
+    GPIO.output(pins[2], GPIO.HIGH)
+    GPIO.output(pins[4], GPIO.HIGH)
+
+def leds_green_on():
+    leds_off()
+    GPIO.output(pins[1], GPIO.HIGH)
+    GPIO.output(pins[3], GPIO.HIGH)
+
+
+def flash_async(e, t):
+    while not e.isSet():
+        leds_green_on()
+        sleep(0.5)
+        leds_off()
+        sleep(0.5)
+
+def flash()
+    e = threading.Event()
+    t = threading.Thread(name='non-block', target=flash_async, args=(e, 2))
+    t.start()
+    return e
 
 def button_callback(channel):
     print('Button was pressed.')
+    restart()
 
 def init():
     GPIO.setwarnings(False)  # Ignore warning for now
     GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
     GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(7, GPIO.RISING, callback=button_callback)  # Setup event on pin 10 rising edge
+    print('Press button on mrbean-robot to get the new version of code from github and restart.')
     input("Press enter to quit\n\n")
--- main.py
+++ main.py
@@ -23,53 +23,50 @@ ar = PiMotor.Arrow(4)
 ## forward and reverse takes speed in percentage(0-100)
 
 try:
-    while True:
 #-----------To Drive the Motors Forward------------# 
-        print("Robot Moving Forward ")
-        af.on()
-        motorAll.forward(100)
-        time.sleep(5)
+    print("Robot Moving Forward ")
+    af.on()
+    motorAll.forward(100)
+    time.sleep(5)
 #--------------------------------------------------#
 
 #-----------To Drive the Motors backwards------------# 
-        print("Robot Moving Backward ")
-        af.off()
-        ab.on()
-        motorAll.reverse(100)
-        time.sleep(5)
+    print("Robot Moving Backward ")
+    af.off()
+    ab.on()
+    motorAll.reverse(100)
+    time.sleep(5)
 #--------------------------------------------------#
 
 #-----------To Drive the Motors Left---------------#
-        print("Robot Moving Left ")
-        ab.off()
-        al.on()
-        m1.stop()
-        m2.stop()
-        m3.forward(100)
-        m4.forward(100)
-        time.sleep(5)
+    print("Robot Moving Left ")
+    ab.off()
+    al.on()
+    m1.stop()
+    m2.stop()
+    m3.forward(100)
+    m4.forward(100)
+    time.sleep(5)
 #--------------------------------------------------#
 
 #----------To Drive the Motors Right---------------#
-        print("Robot Moving Right ")
-        ar.on()
-        al.off()
-        m1.forward(100)
-        m2.forward(100)
-        m3.stop()
-        m4.stop()
-        time.sleep(5)
+    print("Robot Moving Right ")
+    ar.on()
+    al.off()
+    m1.forward(100)
+    m2.forward(100)
+    m3.stop()
+    m4.stop()
+    time.sleep(5)
 #-------------------------------------------------#
 
 #---------To Stop the Motors----------------------#
-        print("Robot Stop ")
-        al.off()
-        af.off()
-        ar.off()
-        motorAll.stop()
-        time.sleep(5)
+    print("Robot Stop ")
+    al.off()
+    af.off()
+    ar.off()
+    motorAll.stop()
+    time.sleep(5)
 #-------------------------------------------------#
-
-        
 except KeyboardInterrupt:
     GPIO.cleanup()
