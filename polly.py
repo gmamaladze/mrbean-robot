35a61af221c26a51838b2c07e6b3ab1d6dafe9d2
George Mamaladze <george@Georges-MacBook-Pro.local>
2019-02-09 21:49:22 +0100

    Sound module refactoring.

--- polly.py
+++ polly.py
@@ -11,16 +11,16 @@ class Polly():
     def __init__(self, voiceId):
         self.polly = boto3.client('polly', region_name='eu-central-1') #access amazon web service
         self.VOICE_ID = voiceId
+        self.mixer = alsaaudio.Mixer('PCM')
+        self.device = alsaaudio.PCM(device='default')
+        self.device.setchannels(1)
+        self.device.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+        self.device.setrate(8000)
+
+    def unmute(self):
+        self.mixer.setvolume(100)
 
     def say(self, textToSpeech): #get polly response and play directly
         pollyResponse = self.polly.synthesize_speech(Text=textToSpeech, OutputFormat='pcm', SampleRate = '8000', VoiceId=self.VOICE_ID)
-        
-        m = alsaaudio.Mixer('PCM')
-        m.setvolume(100)
-        device = alsaaudio.PCM(device='default')
-        device.setchannels(1)
-        device.setformat(alsaaudio.PCM_FORMAT_S16_LE)
-        device.setrate(8000)
         data = pollyResponse['AudioStream'].read()
-        device.write(data)
-        m.setvolume(0)
+        self.device.write(data)
