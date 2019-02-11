c343b826fbaa4c7c9d3a9988df457f08186105eb
George Mamaladze <george@Georges-MacBook-Pro.local>
2019-02-11 23:02:45 +0100

    Implemented LED arrows

--- main.py
+++ main.py
@@ -22,17 +22,14 @@ for i in range(0, len(head.positions)):
 m3 = PiMotor.Motor("MOTOR3",1)
 m4 = PiMotor.Motor("MOTOR4",1)
 
-while True:
-    position = head.next()
-    time.sleep(0.1)
-    distance = sonar.get_distance()
-    distances[position] = (distances[position] + 4 * distance) / 5
-    preferred_direction = distances.index(max(distances))
-    delta_v = (preferred_direction - 5) * 10
-    m3.forward(50 + delta_v)
-    m4.forward(50 - delta_v)
-    if min(distances)<20:
-        emergency_turn()
+# Arrows
+ab = PiMotor.Arrow(1)
+al = PiMotor.Arrow(2)
+af = PiMotor.Arrow(3) 
+ar = PiMotor.Arrow(4)
+
+arrows = [al, af, ar]
+arrow_index = 0
 
 def emergency_turn():
     head.set_position(5)
@@ -43,52 +40,20 @@ def emergency_turn():
         if distance>50:
             break
 
+while True:
+    position = head.next()
+    time.sleep(0.1)
+    distance = sonar.get_distance()
+    distances[position] = (distances[position] + 4 * distance) / 5
+    preferred_direction = distances.index(max(distances))
+    delta_v = (preferred_direction - 5) * 10
+    m3.forward(50 + delta_v)
+    m4.forward(50 - delta_v)
 
-exit(0)
+    if min(distances)<20:
+        emergency_turn()
 
-def head_rotation():
-    pass
+    arrows[arrow_index].off()
+    arrow_index = round(preferred_direction * 3 / len(head.positions))
+    arrows[arrow_index].on()
 
-_thread.start_new_thread(head_rotation)
-
-
-# Pfeile
-ab = PiMotor.Arrow(1)
-al = PiMotor.Arrow(2)
-af = PiMotor.Arrow(3) 
-ar = PiMotor.Arrow(4)
-
-# gerade aus
-af.on()
-m3.forward(100)
-m4.forward(100)
-time.sleep(5)
-
-# rückwärts
-af.off()
-ab.on()
-m3.reverse(100)
-m4.reverse(100)
-time.sleep(5)
-
-# links
-ab.off()
-al.on()
-m3.forward(100)
-m4.reverse(100)
-time.sleep(5)
-
-# rechts
-ar.on()
-al.off()
-m3.reverse(100)
-m4.forward(100)
-time.sleep(5)
-
-# stop
-al.off()
-af.off()
-ar.off()
-m3.stop()
-m4.stop()
-time.sleep(1)
